31486ddda86f91d8c918fc7ed0bb5cddd66b3a3c
Zsolt Sztancsik <hnhfdv@hallgato.uni-neumann.hu>
2025-10-12 14:52:06 +0200

    Done initial interface to physics engine

--- Robotics Simulation/header/physics/BodyDefinition.h
+++ Robotics Simulation/header/physics/BodyDefinition.h
@@ -2,14 +2,52 @@
 #include "core/Vector2.h"
 #include "core/Angle.h" 
 
+enum class BodyType : char
+{
+	Static,
+	Kinematic,
+	Dynamic
+};
+
+struct Shape
+{
+	virtual ~Shape() = default;
+};
+
+struct Circle : Shape
+{
+	float radius;
+};
+
+struct Rectangle : Shape
+{
+	float width, height;
+};
+
 struct BodyDefinition {
-	bool Wheel = false;
+	/* Body definition */
+	Vector2 position = { 0, 0 };
+	Radian rotation = 0.f;
+	Vector2 initialVelocity = { 0, 0 };
+	const char name[32] = "RigidBody";
+
+	BodyType type = BodyType::Static;
+
 	float anglularDamping = 0.f;
 	float linearDamping = 0.f;
-	Vector2 position = { 0, 0 };
-	Angle rotation = 0.f;
-	Vector2 initialVelocity = { 0, 0 };
+
+	bool Wheel = false;
 	bool isRotationFixed = false;
 	bool isBullet = false;
+	float sleepTreshold = 0.05f;
 
+	/* Shape definition */ 
+	float density = 0;			// Mass per unit area in kg/m^2
+	float friction = 0;			// Coefficient of friction, typically between 0 and 1
+	float restitution = 1;		// Bounciness, Coefficent of speed between before and after the collision, typically between 0 and 1
+	float rollingFriction = 0;	// Coefficient of rolling friction, typically between 0 and 1
+	float tangentSpeed = 0;		// Speed at which the shape moves along its tangent, useful for simulating conveyor belts or similar effects
+
+	/* Polygon definition */
+	Shape* shape = nullptr;
 };
--- Robotics Simulation/header/physics/IPhysicsEngine.h
+++ Robotics Simulation/header/physics/IPhysicsEngine.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "physics/World.h"
+#include "physics/BodyDefinition.h"
+#include "box2d/box2cpp.h"
 
 // Interface for a physics engine
+
+using bodyId = unsigned int;
 
 struct IPhysicsEngineBase
 { 
@@ -19,15 +23,28 @@ struct IWorldManager : SimulationLifecycle
 	virtual void simulateStep(float deltaSeconds) = 0;
 };
 
-struct IPrimitivePhysics
+struct IBodyManager : IPhysicsEngineBase
 {
+	virtual bodyId createBody(const BodyDefinition& body) = 0;
+	virtual void destroyBody(bodyId id) = 0;
 
+	virtual Vector2 getBodyPosition(bodyId id) const = 0;
+	virtual Radian getBodyRotation(bodyId id) const = 0;
+
+	virtual void applyForceToBody(bodyId id, const Vector2& force, float timeWindow) = 0;
+	virtual void setSpeed(bodyId id, const Vector2& impulse) = 0;
+};
+
+struct IJointManager : IPhysicsEngineBase
+{
+	// In the future, we can add methods to create and manage joints between bodies
 };
 
 struct IPhysicsEngine :
 	SimulationLifecycle,
 	IWorldManager,
-	IPrimitivePhysics
+	IBodyManager,
+	IJointManager
 {
 
 };
