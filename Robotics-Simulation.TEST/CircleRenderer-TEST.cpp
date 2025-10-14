d8197dd1222a9e7ab0e9ea35d9e323fb77ff53dc
Zsolt Sztancsik <hnhfdv@hallgato.uni-neumann.hu>
2025-10-14 19:34:56 +0200

    Brought tests up to compilable

--- Robotics-Simulation.TEST/CircleRenderer-TEST.cpp
+++ Robotics-Simulation.TEST/CircleRenderer-TEST.cpp
@@ -1,5 +1,5 @@
 #include "pch.h"
-#include "graphics/CircleRenderer.h"
+#include "graphics/CircleRendererComponent.h"
 #include "gtest/gtest.h"
 #include <memory>
 #include "MockLogger.h"
@@ -13,18 +13,23 @@ TEST(CircleRendererTest, UpdateDrawsCircleAtCorrectPosition) {
     MockLogger logger;
     MockRenderer renderer;
     auto gameObject = std::make_unique<MockGameObject>(logger);
+	Camera camera(renderer, logger, { 0, 0 }, { 800, 600 }, { 800, 600 });
     
-    Vector2 testPosition(10.0f, 20.0f);
+    Vector2 testPosition(0, 0);
+
+    // manually calculate testposition in pixel-space
+	Vector2 EcpectedPixelPosition = camera.PixelToWorldPos(testPosition);
+
     gameObject->SetPosition(testPosition);
     
-    CircleRenderer circleRenderer(gameObject.get(), renderer, logger);
+    CircleRendererComponent circleRenderer(gameObject.get(), camera, logger);
     
     // Act
     circleRenderer.Update();
     
     // Assert
     EXPECT_TRUE(renderer.drawCircleCalled);
-    EXPECT_FLOAT_EQ(renderer.lastPosition.x, testPosition.x);
-    EXPECT_FLOAT_EQ(renderer.lastPosition.y, testPosition.y);
+    EXPECT_FLOAT_EQ(renderer.lastPosition.x, EcpectedPixelPosition.x);
+    EXPECT_FLOAT_EQ(renderer.lastPosition.y, EcpectedPixelPosition.y);
     EXPECT_FLOAT_EQ(renderer.lastRadius, 50.0f);
 }
