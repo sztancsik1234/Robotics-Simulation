8dc7a69a935d82f52a711b9c7919c7859ee15f96
Zsolt Sztancsik <hnhfdv@hallgato.uni-neumann.hu>
2025-10-14 19:04:10 +0200

    Added MockViewport

--- Robotics-Simulation.TEST/Mocks.cpp
+++ Robotics-Simulation.TEST/Mocks.cpp
@@ -7,7 +7,48 @@
 #include "util/Exceptions.h"
 #include "MockRenderer.h"
 #include "MockLogger.h"
+#include "MockViewport.h"
 
+
+
+// MockViewport implementation
+Vector2 MockViewport::GetViewCenter() const { return viewCenter; }
+Vector2 MockViewport::GetViewSize() const { return viewSize; }
+Vector2 MockViewport::GetScreenResolution() const { return screenSize; }
+
+Vector2 MockViewport::PixelToWorldPos(Vector2 px, bool snapToPixel) const
+{
+    if (snapToPixel) { px.x = std::round(px.x); px.y = std::round(px.y); }
+    // Simple symmetrical mapping for tests (no Y flip):
+    const Vector2 topLeft{ viewCenter.x - viewSize.x * 0.5f, viewCenter.y - viewSize.y * 0.5f };
+    return {
+        topLeft.x + (px.x * (viewSize.x / screenSize.x)),
+        topLeft.y + (px.y * (viewSize.y / screenSize.y))
+    };
+}
+
+Vector2 MockViewport::WorldToPixelPos(Vector2 wp, bool snapToPixel) const
+{
+    const Vector2 topLeft{ viewCenter.x - viewSize.x * 0.5f, viewCenter.y - viewSize.y * 0.5f };
+    Vector2 px{
+        (wp.x - topLeft.x) * (screenSize.x / viewSize.x),
+        (wp.y - topLeft.y) * (screenSize.y / viewSize.y)
+    };
+    if (snapToPixel) { px.x = std::round(px.x); px.y = std::round(px.y); }
+    return px;
+}
+
+Transform MockViewport::ToScreenSpace(const Transform& world, bool snapToPixel) const
+{
+    Transform t{};
+    t.position = WorldToPixelPos(world.position, snapToPixel);
+    t.size = {
+        std::max(1.f, std::round(world.size.x * (screenSize.x / viewSize.x))),
+        std::max(1.f, std::round(world.size.y * (screenSize.y / viewSize.y)))
+    };
+    t.rotation = world.rotation;
+    return t;
+}
 
 // MockRenderer implementation
 void MockRenderer::DrawCircle(Vector2 position, float radius) {
